f026b2aaf01d4b57d7d58aadeb1927b59fa73477
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2022-12-15 17:38:09 +0000

    Week 12 Application users setup

---
+++ Week12MVCHealthApp2223.S00211628/Migrations/202212151736504_adding roles and users.cs
@@ -0,0 +1,18 @@
+﻿namespace Week12MVCHealthApp2223.S00211628.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingrolesandusers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "EntityID", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "EntityID");
+        }
+    }
+}
--- Week12MVCHealthApp2223.S00211628/Migrations/Configuration.cs
+++ Week12MVCHealthApp2223.S00211628/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 ﻿namespace Week12MVCHealthApp2223.S00211628.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Tracker.WebAPIClient;
+    using Week12MVCHealthApp2223.S00211628.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Week12MVCHealthApp2223.S00211628.Models.ApplicationDbContext>
     {
@@ -14,10 +18,28 @@
 
         protected override void Seed(Week12MVCHealthApp2223.S00211628.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            ActivityAPIClient.Track(StudentID: "S00211628", StudentName: "Martin Melody", activityName: "Rad301 2223 Week 12Lab", Task: "Testing Login MVC App");
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Admin" });
+
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Doctor" });
+
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "LabTech" });
+
+            PasswordHasher ps = new PasswordHasher();
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    EntityID = "Admin",
+                    UserName = "admin@SUH.ie",
+                    Email = "Admin@suh.ie",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    PasswordHash = ps.HashPassword("TheAdmin$1")
+                });
         }
     }
 }
